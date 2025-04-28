65e36566a7ce8f8534a4d48d09db9dea73075c9e
tolga ayvazoglu <tolga.ayvazoglu@bergfreunde.de>
2025-04-28 02:24:43 +0200

    adds missing commands

--- cmd/tracker/resume.go
+++ cmd/tracker/resume.go
@@ -4,6 +4,9 @@ package main
 import (
 	"fmt"
 
+	"github.com/impactj90/lofi-tracker/cmd/internal/db"
+	"github.com/impactj90/lofi-tracker/cmd/internal/git"
+	"github.com/impactj90/lofi-tracker/cmd/internal/tracker"
 	"github.com/spf13/cobra"
 )
 
@@ -15,6 +18,26 @@ var resumeCmd = &cobra.Command{
 	Use:   "resume",
 	Short: "Resume tracking",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("resume tracking...")
+		dbPath := getDBPath()
+		dbConn, err := db.NewSQLiteDB(dbPath)
+		if err != nil {
+			fmt.Printf("Failed to open database: %v\n", err)
+			return
+		}
+
+		branchName, err := git.GetCurrentBranchName()
+		if err != nil {
+			fmt.Printf("Failed to get current branch name: %v\n", err)
+			return
+		}
+
+		tracker := tracker.NewTracker(branchName, dbConn)
+		err = tracker.Resume()
+		if err != nil {
+			fmt.Printf("❌ Failed to pause tracking: %v\n", err)
+			return
+		}
+
+		fmt.Printf("▶️  Session resumed on branch '%s'\n", branchName)
 	},
 }
--- cmd/tracker/status.go
+++ cmd/tracker/status.go
@@ -4,6 +4,9 @@ package main
 import (
 	"fmt"
 
+	"github.com/impactj90/lofi-tracker/cmd/internal/db"
+	"github.com/impactj90/lofi-tracker/cmd/internal/git"
+	"github.com/impactj90/lofi-tracker/cmd/internal/tracker"
 	"github.com/spf13/cobra"
 )
 
@@ -15,6 +18,31 @@ var statusCmd = &cobra.Command{
 	Use:   "status",
 	Short: "Show status",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("status")
+		dbPath := getDBPath()
+		dbConn, err := db.NewSQLiteDB(dbPath)
+		if err != nil {
+			fmt.Printf("Failed to open database: %v\n", err)
+			return
+		}
+
+		branchName, err := git.GetCurrentBranchName()
+		if err != nil {
+			fmt.Printf("Failed to get current branch name: %v\n", err)
+			return
+		}
+
+		t := tracker.NewTracker(branchName, dbConn)
+		status, err := t.Status()
+		if err != nil {
+			fmt.Printf("❌ Failed to get status: %v\n", err)
+			return
+		}
+
+		fmt.Printf("🕒 Total work time: %s\n", tracker.FormatDuration(status.TotalDuration))
+		if status.IsPaused {
+			fmt.Printf("⏸️  Session paused on branch '%s'\n", branchName)
+			return
+		}
+
 	},
 }
